6c46eaa4c469176b4fc17719debe134aa87ad89d
punedo <aurangabadzone3@gmail.com>
2022-03-03 19:25:44 +0100

    Angular Curd with Json server

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentDashComponent } from './student-dash/student-dash.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentDashComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,ReactiveFormsModule,HttpClientModule,BrowserAnimationsModule,  ToastrModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/student-dash/studentDash.model.ts
@@ -0,0 +1,9 @@
+export  class StudentDashModel{
+id:any=0;
+  first:string="";
+last:string="";
+email:string="";
+mobile:number=0;
+fees:number=0
+
+}
